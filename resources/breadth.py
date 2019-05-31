52a058ef97493da35bf278f826ec03e9982b3821
Zachary Pinto <zpinto@uci.edu>
2019-05-31 00:29:55 -0700

    implement breadth and breadth list

--- resources/breadth.py
+++ resources/breadth.py
@@ -1,12 +1,20 @@
 from flask_restful import Resource
 from scrapy import get_class_info, get_select
+from config import BASE_URL, REQUIRED_ARGS
 
 
 class Breadth(Resource):
     def get(self, option):
-        get_class_info()
+        args = REQUIRED_ARGS
+        args['Breadth'] = option
+        classes = get_class_info(BASE_URL, "", args)
+
+        if classes:
+            return classes, 200
+        else:
+            return {'message': 'Cannot find classes'}, 404
 
 
 class BreadthList(Resource):
     def get(self):
-        return get_select('Breadth')
+        return get_select('Breadth'), 200
