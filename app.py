d0356f4f9a0941af4f09b66897bd909ae1de1c74
Zachary Pinto <zpinto@uci.edu>
2019-05-31 01:30:45 -0700

    update app with new resources

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 
+from resources.filter import FilterList
 from resources.breadth import Breadth, BreadthList
+from resources.dept import Dept, DeptList
+from resources.yearterm import YearTermList
+from resources.division import DivisionList
+from resources.course import Course
 
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(FilterList, '/filters')
 api.add_resource(Breadth, '/Breadth/<string:option>')
 api.add_resource(BreadthList, '/Breadths')
+api.add_resource(Dept, '/Dept/<string:option>')
+api.add_resource(DeptList, '/Depts')
+api.add_resource(YearTermList, '/YearTerms')
+api.add_resource(DivisionList, '/Divisions')
+api.add_resource(Course, '/Class')
+
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
