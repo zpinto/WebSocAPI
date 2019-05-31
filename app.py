6c2f42d47f4398e352b8ff2485c7eae0c2d78ab7
Zachary Pinto <zpinto@uci.edu>
2019-05-31 00:30:17 -0700

    add resources

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, jsonify
 from flask_restful import Api
 
+from resources.breadth import Breadth, BreadthList
+
 app = Flask(__name__)
 api = Api(app)
 
+api.add_resource(Breadth, '/Breadth/<string:option>')
+api.add_resource(BreadthList, '/Breadths')
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
